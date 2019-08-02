b8f25a29b950e1ad0d50edd481014d2886b7ee68
Danushka2 <dhanushkanuwan24@gmail.com>
2019-08-02 23:33:36 +0530

    testing

--- app/src/androidTest/java/com/example/projectmad/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/projectmad/ExampleInstrumentedTest.java
@@ -1,8 +1,9 @@
-package com.example.student.lab2;
+package com.example.projectmad;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+
+import androidx.test.InstrumentationRegistry;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.student.lab2", appContext.getPackageName());
+        assertEquals("com.example.projectmad", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/projectmad/ExampleUnitTest.java
+++ app/src/test/java/com/example/projectmad/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.student.lab2;
+package com.example.projectmad;
 
 import org.junit.Test;
 
